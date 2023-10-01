8a0bf1bd9d7ea1add0369375d005815e776e717b
Arun Kumar C <arunkumarmallow@gmail.com>
2023-10-01 16:27:38 +0530

    login setup completed

--- app/controllers/onboarding/wizard_controller.rb
+++ app/controllers/onboarding/wizard_controller.rb
@@ -1,31 +1,29 @@
 # frozen_string_literal: true
 
-module Onboarding
-  class WizardController < AuthenticatedController
-    STEPS = %w[Basics Verification].freeze
-    TOTAL_STEPS = STEPS.count
+class Onboarding::WizardController < ApplicationController
+  STEPS = %w[Basics Verification].freeze
+  TOTAL_STEPS = STEPS.count
 
-    def prepare_step
-      @account = current_account
-      @steps = (1..TOTAL_STEPS)
-      @onboarding_step = @steps.find { |x| x == params[:step].to_i } || 1
-      @onboarding_step_name = STEPS[@onboarding_step - 1]
-      @onboarding_step_key = @onboarding_step_name.parameterize(separator: '_')
-    end
-
-    def show
-      prepare_step
-
-      prepare_method_name = "prepare_#{@onboarding_step_key}".to_sym
-      send(prepare_method_name) if respond_to? prepare_method_name
-
-      render 'show'
-    end
-
-    def update; end
-
-    private
-
-    def prepare_verification; end
+  def prepare_step
+    @account = current_account
+    @steps = (1..TOTAL_STEPS)
+    @onboarding_step = @steps.find { |x| x == params[:step].to_i } || current_account&.onboard_step || 1
+    @onboarding_step_name = STEPS[@onboarding_step - 1]
+    @onboarding_step_key = @onboarding_step_name.parameterize(separator: '_')
   end
+
+  def show
+    prepare_step
+
+    prepare_method_name = "prepare_#{@onboarding_step_key}".to_sym
+    send(prepare_method_name) if respond_to? prepare_method_name
+
+    render 'show'
+  end
+
+  def update; end
+
+  private
+
+  def prepare_verification; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   namespace :onboarding do
     get  'steps/:step',  to: 'wizard#show', as: 'step'
     post 'steps/:step',  to: 'wizard#update'
   end
+
+  root 'dashboard#show'
 end
