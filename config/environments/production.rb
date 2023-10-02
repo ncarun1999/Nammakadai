a0c02f9c03712a30462263b87554888cfef439c8
Arun Kumar C <arunkumarmallow@gmail.com>
2023-10-02 11:47:06 +0530

    smtp setting added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'localhost:3000',
+    user_name: 'namakadai.shop@gmail.com',
+    password: 'nmkqiujekdepzins',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5 }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
