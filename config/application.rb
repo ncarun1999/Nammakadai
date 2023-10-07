367bb52d0a419d60d3a80acd5b9293498eb9ac2f
Arun Kumar C <arunkumarmallow@gmail.com>
2023-10-07 14:05:02 +0530

    fix 1 added

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module My1
+module Namakadai
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
