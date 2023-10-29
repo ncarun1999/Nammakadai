8d0314e1fc5f0035f001073e0fa39074331e58b0
Arun Kumar C <arunkumarmallow@gmail.com>
2023-10-29 13:53:21 +0530

    file upload css changed

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'flowbite', to: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.turbo.min.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin "tailwindcss-stimulus-components" # @4.0.4
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
